324191c829c7b54cd05367a025ffbbadd61e9ab7
valeryyakishkin <velyakishkin@gmail.com>
2023-05-10 17:54:33 +0300

    adding initial app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        },
+      ]);
+      setText("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" />
+            <span>{todo.text}</span>
+            <span style={{ color: "red" }}>&times;</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
